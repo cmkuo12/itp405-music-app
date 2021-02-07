1fee7fb7c476aefff32b162c81c3a8f251102f76
Chloe <cmkuo@usc.edu>
2021-02-07 10:44:28 -0800

    Fixed spelling

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,7 +13,7 @@
             <div class="col-3">
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('playlist.index')}}">Playlist</a>
+                        <a class="nav-link" href="{{route('playlist.index')}}">Playlists</a>
                     </li>
                 </ul>
             </div>
